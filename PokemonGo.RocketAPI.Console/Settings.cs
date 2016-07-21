0feac8b3e0eb16b471769d6ca6115765b9511166
FeroxRev <Ferox2552@gmail.com>
2016-07-21 14:15:17 +0200

    Refactor persisting settings by using .settings, so that GoogleRefreshToken is not overwritten on new build

--- PokemonGo.RocketAPI.Console/Settings.cs
+++ PokemonGo.RocketAPI.Console/Settings.cs
@@ -9,26 +9,19 @@ namespace PokemonGo.RocketAPI.Console
 {
     public class Settings : ISettings
     {
-        public AuthType AuthType => (AuthType)Enum.Parse(typeof(AuthType), GetSetting());
-        public  string PtcUsername => GetSetting() != string.Empty ? GetSetting() : "username";
-        public  string PtcPassword => GetSetting() != string.Empty? GetSetting() : "password";
-        public double DefaultLatitude => GetSetting() != string.Empty ? double.Parse(GetSetting(), CultureInfo.InvariantCulture) : 52.379189; //Default Amsterdam Central Station
-        public double DefaultLongitude => GetSetting() != string.Empty ? double.Parse(GetSetting(),CultureInfo.InvariantCulture) : 4.899431;//Default Amsterdam Central Station
+        public AuthType AuthType => (AuthType)Enum.Parse(typeof(AuthType), UserSettings.Default.AuthType);
+        public string PtcUsername => UserSettings.Default.PtcUsername;
+        public string PtcPassword => UserSettings.Default.PtcPassword;
+        public double DefaultLatitude => UserSettings.Default.DefaultLatitude;
+        public double DefaultLongitude => UserSettings.Default.DefaultLongitude;
         public  string GoogleRefreshToken
         {
-            get { return GetSetting() != string.Empty ? GetSetting() : string.Empty; }
-            set { SetSetting(value); }
-        }
-
-        private string GetSetting([CallerMemberName]string key = null)
-        {
-            return ConfigurationManager.AppSettings[key];
-        }
-        private void SetSetting(string value, [CallerMemberName]string key = null)
-        {
-            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            if (key != null) configFile.AppSettings.Settings[key].Value = value;
-            configFile.Save();
+            get { return UserSettings.Default.GoogleRefreshToken; }
+            set
+            {
+                UserSettings.Default.GoogleRefreshToken = value; 
+                UserSettings.Default.Save();
+            }
         }
     }
 }
