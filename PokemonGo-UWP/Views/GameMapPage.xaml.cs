04523b4ad950b3f551d9938b1d904da7bffb1932
STApps <stapps@outlook.it>
2016-07-22 20:01:48 +0200

    Almost done basic UI for game page

--- PokemonGo-UWP/Views/GameMapPage.xaml.cs
+++ PokemonGo-UWP/Views/GameMapPage.xaml.cs
@@ -8,6 +8,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -20,16 +21,19 @@ namespace PokemonGo_UWP.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
+    /// TODO: bottom bar blur on AU http://stackoverflow.com/questions/36276856/uwp-app-realtime-blur-background-using-dx-compositor/36441888#36441888
     /// </summary>
     public sealed partial class GameMapPage : Page
     {
         public GameMapPage()
         {
             this.InitializeComponent();
-            MapControl.Center = new Geopoint(new BasicGeoposition() {
+            MapControl.Center = new Geopoint(new BasicGeoposition()
+            {
                 Latitude = 48.8530,
                 Longitude = 2.3498
             });
-        }        
+        }
+
     }
 }
