99a5931acc64a92d3b64370da5dcfdc2bc616c88
STApps <stapps@outlook.it>
2016-07-30 11:08:14 +0200

    Probably solved unexpected server response error

--- PokemonGoAPI/Extensions/HttpClientExtensions.cs
+++ PokemonGoAPI/Extensions/HttpClientExtensions.cs
@@ -8,22 +8,50 @@ namespace PokemonGo.RocketAPI.Extensions
 {
     public static class HttpClientExtensions
     {
+
+        private static bool waitingForResponse = false;
+
+        //public static async Task<TResponsePayload> PostProtoPayload<TRequest, TResponsePayload>(this HttpClient client,
+        //    string url, TRequest request) where TRequest : IMessage<TRequest>
+        //    where TResponsePayload : IMessage<TResponsePayload>, new()
+        //{
+        //    Logger.Write($"Requesting {typeof(TResponsePayload).Name}", LogLevel.Debug);
+        //    var response = await PostProto(client, url, request);
+
+        //    if (response.Payload.Count == 0)
+        //        throw new InvalidResponseException();
+
+        //    //Decode payload
+        //    //todo: multi-payload support
+        //    var payload = response.Payload[0];
+        //    var parsedPayload = new TResponsePayload();
+        //    parsedPayload.MergeFrom(payload);
+
+        //    return parsedPayload;
+        //}
+
         public static async Task<TResponsePayload> PostProtoPayload<TRequest, TResponsePayload>(this HttpClient client,
-            string url, TRequest request) where TRequest : IMessage<TRequest>
-            where TResponsePayload : IMessage<TResponsePayload>, new()
+    string url, TRequest request) where TRequest : IMessage<TRequest>
+    where TResponsePayload : IMessage<TResponsePayload>, new()
         {
-            Logger.Write($"Requesting {typeof(TResponsePayload).Name}", LogLevel.Debug);
-            var response = await PostProto(client, url, request);
+            while (waitingForResponse)
+                await Task.Delay(30);
+            waitingForResponse = true;
 
-            if (response.Payload.Count == 0)
-                throw new InvalidResponseException();
+            Response response;
+            var count = 0;
+            do
+            {
+                count++;                
+                response = await PostProto(client, url, request);
+                waitingForResponse = false;
 
-            //Decode payload
-            //todo: multi-payload support
+                await Task.Delay(30);// request every 30ms, up this value for not spam their server
+            } while (response.Payload.Count < 1 && count < 30);
+
             var payload = response.Payload[0];
             var parsedPayload = new TResponsePayload();
             parsedPayload.MergeFrom(payload);
-
             return parsedPayload;
         }
 
@@ -31,8 +59,7 @@ namespace PokemonGo.RocketAPI.Extensions
             where TRequest : IMessage<TRequest>
         {
             //Encode payload and put in envelop, then send
-            var data = request.ToByteString();
-            await Task.Delay(1000);
+            var data = request.ToByteString();            
             var result = await client.PostAsync(url, new ByteArrayContent(data.ToByteArray()));
 
             //Decode message
