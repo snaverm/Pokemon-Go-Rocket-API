3ecd6c5ea40393f56d11de7cdaa30b4f9da93015
Matthias Heilhecker <m.heilhecker@mh-dev.net>
2016-08-03 20:54:59 +0200

    #312: Translated Pokemons to german
    Added first UI translations

--- PokemonGo-UWP/Utils/Resources.cs
+++ PokemonGo-UWP/Utils/Resources.cs
@@ -4,5 +4,6 @@ using Windows.ApplicationModel.Resources;
 namespace PokemonGo_UWP.Utils {
     public static class Resources {
         public static ResourceLoader Pokemon = ResourceLoader.GetForCurrentView("Pokemon");
+        public static ResourceLoader Translation = ResourceLoader.GetForCurrentView("Resources");
     }
 }
