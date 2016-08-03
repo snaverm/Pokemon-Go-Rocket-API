73c662e6ff94d3f2b299bd9728583e61341ae362
Matthias Heilhecker <m.heilhecker@mh-dev.net>
2016-08-03 20:22:49 +0200

    #312: Added Resource file for Pokemon
    Now, Pokemons can be translated

---
+++ PokemonGo-UWP/Utils/Resources.cs
@@ -0,0 +1,8 @@
+﻿
+using Windows.ApplicationModel.Resources;
+
+namespace PokemonGo_UWP.Utils {
+    public static class Resources {
+        public static ResourceLoader Pokemon = ResourceLoader.GetForCurrentView("Pokemon");
+    }
+}
