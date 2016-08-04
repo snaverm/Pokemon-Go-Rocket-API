43c52c7b2c69682998d3b9f82aa9b2c690881ab3
Skyne <skyne9@gmail.com>
2016-08-04 09:13:37 +0200

    wrap login response if LoginFailedException has been thrown, so we can discover all the possible error causes

--- PokemonGoAPI/Exceptions/LoginFailedException.cs
+++ PokemonGoAPI/Exceptions/LoginFailedException.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PokemonGo.RocketAPI.Exceptions
 {
     public class LoginFailedException : Exception
     {
+        public LoginFailedException(HttpResponseMessage loginResponse)
+        {
+            this.LoginResponse = loginResponse;
+        }
+        public HttpResponseMessage LoginResponse { get; private set; }
+
+        public string LoginResponseContentString
+        {
+            get
+            {
+                return Task.Run(async () => await this.LoginResponse.Content.ReadAsStringAsync()).Result;
+            }
+        }
     }
 }
