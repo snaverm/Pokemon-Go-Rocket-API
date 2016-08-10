acb4ffb9aaec0b2ff6bf5e382dd8e12db98a999b
Robert McLaws <robert@advancedrei.com>
2016-08-10 19:10:46 -0400

    SortByName fix
    In the official app, when Pokemon are sorted by name, they are then also
    sorted by CP in descending order.

--- PokemonGo-UWP/ViewModels/PokemonInventoryPageViewModel.cs
+++ PokemonGo-UWP/ViewModels/PokemonInventoryPageViewModel.cs
@@ -185,7 +185,7 @@ namespace PokemonGo_UWP.ViewModels
                 case PokemonSortingModes.Health:
                     return pokemonInventory.OrderByDescending(pokemon => pokemon.Stamina);
                 case PokemonSortingModes.Name:
-                    return pokemonInventory.OrderBy(pokemon => Utils.Resources.Pokemon.GetString(pokemon.PokemonId.ToString()));
+                    return pokemonInventory.OrderBy(pokemon => Utils.Resources.Pokemon.GetString(pokemon.PokemonId.ToString())).ThenByDescending(pokemon => pokemon.Cp);
                 case PokemonSortingModes.Combat:
                     return pokemonInventory.OrderByDescending(pokemon => pokemon.Cp);
                 default:
