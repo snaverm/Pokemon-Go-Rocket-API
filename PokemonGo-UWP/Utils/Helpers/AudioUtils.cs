64aed725e0a8ae27ebd9b5d4f89c4bd1eecad8a0
christosk92 <myproductsofficial@outlook.com>
2016-08-17 22:13:57 +0200

    Updated audioutils

--- PokemonGo-UWP/Utils/Helpers/AudioUtils.cs
+++ PokemonGo-UWP/Utils/Helpers/AudioUtils.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.Storage;
 using Windows.UI.Xaml.Controls;
@@ -8,16 +8,41 @@ namespace PokemonGo_UWP.Utils
 {
     public static class AudioUtils
     {
+        public static MediaElement mysong1 = new MediaElement();
+        public static MediaElement mysong = new MediaElement();
         public static async Task PlaySound(string asset)
         {
-            var mysong = new MediaElement();
-            var folder =
-                await (await Package.Current.InstalledLocation.GetFolderAsync("Assets")).GetFolderAsync("Audio");
-            var file = await folder.GetFileAsync(asset);
-            var stream = await file.OpenAsync(FileAccessMode.Read);
-            mysong.Volume = 1;
-            mysong.SetSource(stream, file.ContentType);
-            mysong.Play();
+            if (SettingsService.Instance.IsMusicEnabled)
+            {
+                if((string)asset == "Gameplay.mp3")
+                {
+                    mysong1.IsLooping = true;
+                }
+                var folder =
+                       await (await Package.Current.InstalledLocation.GetFolderAsync("Assets")).GetFolderAsync("Audio");
+                var file = await folder.GetFileAsync(asset);
+                var stream = await file.OpenAsync(FileAccessMode.Read);
+                mysong1.Volume = 1;
+                mysong1.SetSource(stream, file.ContentType);
+                mysong1.Play();
+                            
+            }
+        }
+
+        public static async Task PlaySoundCapture(string asset)
+        {
+            if (SettingsService.Instance.IsMusicEnabled)
+            {
+                mysong1.Pause();
+
+                var folder =
+                    await (await Package.Current.InstalledLocation.GetFolderAsync("Assets")).GetFolderAsync("Audio");
+                var file = await folder.GetFileAsync(asset);
+                var stream = await file.OpenAsync(FileAccessMode.Read);
+                mysong.Volume = 1;
+                mysong.SetSource(stream, file.ContentType);
+                mysong.Play();
+            }
         }
     }
 }
