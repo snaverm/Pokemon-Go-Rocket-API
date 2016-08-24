f4c58c0711d6dd62c2f7fd7d1d1a1238367dbea9
Rover656 <reece-mackie@hotmail.com>
2016-08-24 10:51:41 +0100

    Update GameMapPage.xaml.cs

--- PokemonGo-UWP/Views/GameMapPage.xaml.cs
+++ PokemonGo-UWP/Views/GameMapPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Windows.Devices.Geolocation;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -52,25 +52,11 @@ namespace PokemonGo_UWP.Views
                     Margin = new Thickness(0,8,0,0),
                     Content = new Image()
                     {
-                    	var currentTime = int.Parse(DateTime.Now.ToString("HH"));
-                        if (currentTime > 7 && currentTime < 19) {
-                        	var colorSchemeRecenterMapIcon = "Light";
-                        } else {
-                                var colorSchemeRecenterMapIcon = "Dark";
-                        }
-                        if (colorSchemeRecenterMapIcon == "Light") {
-                        	Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/Icons/RecenterMapIconLight.png") },
-                        	Stretch = Stretch.Uniform,
-                		Height = 36,
-                        	HorizontalAlignment = HorizontalAlignment.Center,
-                        	VerticalAlignment                        = VerticalAlignment.Center
-                        } else {
-                        	Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/Icons/RecenterMapIconDark.png") },
-                        	Stretch = Stretch.Uniform,
-                        	Height = 36,
-                        	HorizontalAlignment = HorizontalAlignment.Center,
-                		VerticalAlignment                        = VerticalAlignment.Center
-                        }
+                        Source = new BitmapImage() { UriSource = new Uri($"ms-appx:///Assets/Icons/RecenterMapIcon{ViewModel.CurrentTheme}.png")) },
+                        Stretch = Stretch.Uniform,
+                        Height = 36,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment                        = VerticalAlignment.Center
                     }
                 };
                 ReactivateMapAutoUpdateButton.Tapped += ReactivateMapAutoUpdate_Tapped;
