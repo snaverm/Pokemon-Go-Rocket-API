8b0bfe40e41b0a285076804378822e20cac12ebd
pinoelefante <pinoelefante@hotmail.it>
2016-08-24 11:17:33 +0200

    fix to margins

--- PokemonGo-UWP/Views/PokedexPage.xaml.cs
+++ PokemonGo-UWP/Views/PokedexPage.xaml.cs
@@ -1,26 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Core;
+﻿using PokemonGo_UWP.Utils;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace PokemonGo_UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PokedexPage : Page
     {
         public PokedexPage()
@@ -31,16 +17,20 @@ namespace PokemonGo_UWP.Views
         {
             ViewModel.PropertyChanged += ViewModel_PropertyChanged;
         }
-
         private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if(e.PropertyName.CompareTo(nameof(ViewModel.SelectedPokedexEntry)) == 0)
                 scrollPokedexEntry.ChangeView(0, 0, scrollPokedexEntry.ZoomFactor);
         }
-
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
+        }
+        private WidthConverter widthCalc = new WidthConverter();
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var newWidth = (int)widthCalc.Convert(1, null, "0,28", null);
+            pokeindex.Width = newWidth;
         }
     }
 }
