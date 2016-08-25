ab05e743fcf3ba42d8252b90aa741d7d5ef56485
Raphael Zenhäusern <raphael@zenhausern.net>
2016-08-26 00:35:02 +0200

    Implemented Item Recycle

---
+++ PokemonGo-UWP/Controls/MessageDialog/StepperMessageDialog.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace PokemonGo_UWP.Controls
+{
+    public sealed partial class StepperMessageDialog : UserControl
+    {
+        public StepperMessageDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        public StepperMessageDialog(int minValue, int maxValue, int value) :this()
+        {
+            MinValue = minValue;
+            MaxValue = maxValue;
+            Value = value;
+        }
+
+        #region Propertys
+
+        public static readonly DependencyProperty ValueProperty = 
+            DependencyProperty.Register(nameof(Value), typeof(int), typeof(PoGoMessageDialog),
+                new PropertyMetadata(1));
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        private int _minValue;
+        public int MinValue
+        {
+            get { return _minValue; }
+            set { _minValue = value; }
+        }
+
+        private int _maxValue;
+        public int MaxValue
+        {
+            get { return _maxValue; }
+            set { _maxValue = value; }
+        }
+
+        #endregion
+
+        #region Events
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            if(Value > MinValue)
+            {
+                Value--;
+            }
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            if(Value < MaxValue)
+            {
+                Value++;
+            }
+        }
+
+        #endregion
+    }
+}
--- PokemonGo-UWP/Utils/GameClient.cs
+++ PokemonGo-UWP/Utils/GameClient.cs
@@ -673,7 +673,7 @@ namespace PokemonGo_UWP.Utils
         /// <summary>
         ///     List of items that can be used when trying to catch a Pokemon
         /// </summary>
-        private static readonly List<ItemId> CatchItemIds = new List<ItemId>
+        public static readonly List<ItemId> CatchItemIds = new List<ItemId>
         {
             ItemId.ItemPokeBall,
             ItemId.ItemGreatBall,
@@ -684,6 +684,24 @@ namespace PokemonGo_UWP.Utils
             ItemId.ItemRazzBerry,
             ItemId.ItemUltraBall,
             ItemId.ItemWeparBerry
+        };
+
+        /// <summary>
+        /// List of items, that can be used from the normal ItemsInventoryPage
+        /// </summary>
+        public static readonly List<ItemId> NormalUseItemIds = new List<ItemId>
+        {
+            ItemId.ItemPotion,
+            ItemId.ItemSuperPotion,
+            ItemId.ItemHyperPotion,
+            ItemId.ItemMaxPotion,
+            ItemId.ItemRevive,
+            ItemId.ItemMaxRevive,
+            ItemId.ItemLuckyEgg,
+            ItemId.ItemIncenseOrdinary,
+            ItemId.ItemIncenseSpicy,
+            ItemId.ItemIncenseCool,
+            ItemId.ItemIncenseFloral,
         };
 
         /// <summary>
@@ -976,6 +994,21 @@ namespace PokemonGo_UWP.Utils
         public static async Task<FortSearchResponse> SearchFort(string pokestopId, double latitude, double longitude)
         {
             return await _client.Fort.SearchFort(pokestopId, latitude, longitude);
+        }
+
+        #endregion
+
+        #region Items Handling
+
+        /// <summary>
+        ///     Recycles the given amount of the selected item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static async Task<RecycleInventoryItemResponse> RecycleItem(ItemId item, int amount)
+        {
+            return await _client.Inventory.RecycleItem(item, amount);
         }
 
         #endregion
