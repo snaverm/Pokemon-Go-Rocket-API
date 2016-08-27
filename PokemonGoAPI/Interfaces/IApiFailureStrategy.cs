4b0635671416994da5d7cdc6afce2430e0b816cc
Robert McLaws <robert@advancedrei.com>
2016-08-27 18:04:22 -0400

    Remove and sort on IApiFailureStrategy

--- PokemonGoAPI/Interfaces/IApiFailureStrategy.cs
+++ PokemonGoAPI/Interfaces/IApiFailureStrategy.cs
@@ -1,10 +1,5 @@
-﻿using POGOProtos.Networking.Envelopes;
-using PokemonGoAPI.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using POGOProtos.Networking.Envelopes;
 
 namespace PokemonGoAPI.Interfaces
 {
