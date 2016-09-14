8184792389bdec73b18434d640d7acf494c56a87
mtaheij <michel@taheij.nl>
2016-09-14 12:26:18 +0200

    Ability to install a Pokestop module (Troy disc)

--- PokemonGo-UWP/ViewModels/SearchPokeStopPageViewModel.cs
+++ PokemonGo-UWP/ViewModels/SearchPokeStopPageViewModel.cs
@@ -20,6 +20,15 @@ namespace PokemonGo_UWP.ViewModels
 {
     public class SearchPokeStopPageViewModel : ViewModelBase
     {
+        #region ctor
+
+        public SearchPokeStopPageViewModel()
+        {
+            SelectedModifierItem = SelectAvailableModifier();
+        }
+
+        #endregion
+
         #region Lifecycle Handlers
 
         /// <summary>
@@ -36,11 +45,17 @@ namespace PokemonGo_UWP.ViewModels
                 // Recovering the state     
                 CurrentPokestopInfo = new FortDetailsResponse();           
                 CurrentSearchResponse = new FortSearchResponse();
+                SelectedModifierItem = new ItemData();
+                CurrentAddModifierResponse = new AddFortModifierResponse();
                 CurrentPokestop = JsonConvert.DeserializeObject<FortDataWrapper>((string)suspensionState[nameof(CurrentPokestop)]);
                 CurrentPokestopInfo.MergeFrom(ByteString.FromBase64((string)suspensionState[nameof(CurrentPokestop)]).CreateCodedInput());
                 CurrentSearchResponse.MergeFrom(ByteString.FromBase64((string)suspensionState[nameof(CurrentSearchResponse)]).CreateCodedInput());
+                SelectedModifierItem.MergeFrom(ByteString.FromBase64((string)suspensionState[nameof(SelectedModifierItem)]).CreateCodedInput());
+                CurrentAddModifierResponse.MergeFrom(ByteString.FromBase64((string)suspensionState[nameof(CurrentAddModifierResponse)]).CreateCodedInput());
                 RaisePropertyChanged(() => CurrentPokestopInfo);
                 RaisePropertyChanged(() => CurrentSearchResponse);
+                RaisePropertyChanged(() => SelectedModifierItem);
+                RaisePropertyChanged(() => CurrentAddModifierResponse);
             }
             else
             {
@@ -74,6 +89,8 @@ namespace PokemonGo_UWP.ViewModels
                 suspensionState[nameof(CurrentPokestop)] = JsonConvert.SerializeObject(CurrentPokestop);
                 suspensionState[nameof(CurrentPokestopInfo)] = CurrentPokestopInfo.ToByteString().ToBase64();
                 suspensionState[nameof(CurrentSearchResponse)] = CurrentSearchResponse.ToByteString().ToBase64();
+                suspensionState[nameof(SelectedModifierItem)] = SelectedModifierItem.ToByteString().ToBase64();
+                suspensionState[nameof(CurrentAddModifierResponse)] = CurrentAddModifierResponse.ToByteString().ToBase64();
             }
             await Task.CompletedTask;
         }
@@ -103,9 +120,24 @@ namespace PokemonGo_UWP.ViewModels
         /// </summary>
         private FortSearchResponse _currentSearchResponse = new FortSearchResponse();
 
+        /// <summary>
+        ///     Current item for modifying a Pokestop
+        /// </summary>
+        private ItemData _selectedModifierItem;
+
+        /// <summary>
+        ///     Results of the current Pokestop modifier addition
+        /// </summary>
+        private AddFortModifierResponse _currentAddModifierResponse = new AddFortModifierResponse();
+
         #endregion
 
         #region Bindable Game Vars
+
+        /// <summary>
+        ///     Reference to global inventory
+        /// </summary>
+        public ObservableCollection<ItemData> ItemsInventory => GameClient.CatchItemsInventory;
 
         /// <summary>
         ///     Pokestop that the user is visiting
@@ -135,10 +167,62 @@ namespace PokemonGo_UWP.ViewModels
         }
 
         /// <summary>
+        ///     Results of the current Pokestop modifier addition
+        /// </summary>
+        public AddFortModifierResponse CurrentAddModifierResponse
+        {
+            get { return _currentAddModifierResponse; }
+            set { Set(ref _currentAddModifierResponse, value); }
+        }
+
+        /// <summary>
         ///     Items awarded by Pokestop searching
         /// </summary>
         public ObservableCollection<ItemAward> AwardedItems { get; } = new ObservableCollection<ItemAward>();
 
+        /// <summary>
+        ///     Current item for modifying pokestop
+        /// </summary>
+        public ItemData SelectedModifierItem
+        {
+            get { return _selectedModifierItem; }
+            set { Set(ref _selectedModifierItem, value); }
+        }
+
+        public string LureTitleText
+        {
+            get
+            {
+                if (CurrentPokestopInfo.Modifiers.Count == 0)
+                {
+					return Utils.Resources.CodeResources.GetString("LureTitleTextEmpty");
+                }
+                else
+                {
+                    return Utils.Resources.CodeResources.GetString("LureTitleTextFilled");
+				}
+            }
+        }
+
+        public string LureDetailsText
+        {
+            get
+            {
+                if (CurrentPokestopInfo.Modifiers.Count == 0)
+                {
+                    return Utils.Resources.CodeResources.GetString("LureDetailsTextEmpty");
+				}
+                else
+                {
+					return Utils.Resources.CodeResources.GetString("LureDetailsTextFilled");
+				}
+			}
+        }
+
+        public bool IsPokestopLured
+        {
+            get { return CurrentPokestopInfo.Modifiers.Count > 0; }
+        }
         #endregion
 
         #region Game Logic
@@ -266,6 +350,134 @@ namespace PokemonGo_UWP.ViewModels
                         throw new ArgumentOutOfRangeException();
                 }
             }, () => true));
+
+        #region Add modifier Events
+
+        /// <summary>
+        ///     Event fired if the user wants to see the Pokestop modifier details
+        /// </summary>
+        public event EventHandler ShowModifierDetails;
+
+        /// <summary>
+        ///     Event fired if the user wants to close the Pokestop modifier details
+        /// </summary>
+        public event EventHandler HideModifierDetails;
+
+        /// <summary>
+        ///     Event fired if the user was able to add a modifier to the Pokestop
+        /// </summary>
+        public event EventHandler AddModifierSuccess;
+
+        /// <summary>
+        ///     Event fired if the user tried to add a modifier to a Pokestop which is too far away
+        /// </summary>
+        public event EventHandler AddModifierTooFarAway;
+
+        /// <summary>
+        ///     Event fired if the Pokestop already has a modifier
+        /// </summary>
+        public event EventHandler AddModifierAlreadyHasModifier;
+
+        /// <summary>
+        ///     Event fired if the Player's inventory does not have the correct item to add to the Pokestop
+        /// </summary>
+        public event EventHandler AddModifierNoItemInInventory;
+
+        #endregion
+
+        private DelegateCommand _showFortModifierDetails;
+
+        public DelegateCommand ShowFortModifierDetails => _showFortModifierDetails ?? (
+            _showFortModifierDetails = new DelegateCommand(async() =>
+            {
+                Busy.SetBusy(true, "Getting Pokestop module details");
+                Logger.Write($"showing modifier details for {CurrentPokestopInfo.Name} [ID = {CurrentPokestop.Id}]");
+                CurrentPokestopInfo =
+                    await GameClient.GetFort(CurrentPokestop.Id, CurrentPokestop.Latitude, CurrentPokestop.Longitude);
+                ShowModifierDetails?.Invoke(this, null);
+                Busy.SetBusy(false);
+            }, () => true));
+
+        private DelegateCommand _hideFortModifierDetails;
+
+        public DelegateCommand HideFortModifierDetails => _hideFortModifierDetails ?? (
+            _hideFortModifierDetails = new DelegateCommand(async () =>
+            {
+                Busy.SetBusy(true, String.Empty);
+                Logger.Write($"hiding modifier details for {CurrentPokestopInfo.Name} [ID = {CurrentPokestop.Id}]");
+                HideModifierDetails?.Invoke(this, null);
+                Busy.SetBusy(false);
+            }, () => true));
+
+        private DelegateCommand _addFortModifier;
+
+        /// <summary>
+        ///     Adds a modifier to the current PokeStop, like a lure module
+        /// </summary>
+        public DelegateCommand AddFortModifier => _addFortModifier ?? (
+            _addFortModifier = new DelegateCommand(async () =>
+            {
+                Busy.SetBusy(true, "Adding module to PokeStop");
+                Logger.Write($"Adding modifier {CurrentPokestopInfo.Name} [ID = {CurrentPokestop.Id}]");
+                CurrentAddModifierResponse =
+                    await GameClient.AddFortModifier(CurrentPokestop.Id, SelectedModifierItem.ItemId);
+                Busy.SetBusy(false);
+                switch (CurrentAddModifierResponse.Result)
+                {
+                    case AddFortModifierResponse.Types.Result.NoResultSet:
+                        break;
+                    case AddFortModifierResponse.Types.Result.Success:
+                        // Success, we play the animation
+                        Logger.Write("Adding Pokestop modifier success");
+                        AddModifierSuccess?.Invoke(this, null);
+                        await GameClient.UpdateInventory();
+                        break;
+                    case AddFortModifierResponse.Types.Result.TooFarAway:
+                        // PokeStop can't be modified because it's too far away, there's nothing that we can do
+                        Logger.Write("Adding Pokestop modifier too far away");
+                        AddModifierTooFarAway?.Invoke(this, null);
+                        break;
+                    case AddFortModifierResponse.Types.Result.FortAlreadyHasModifier:
+                        // PokeStop can't be modified because it already has a modifier, there's nothing that we can do
+                        Logger.Write("Adding Pokestop already has modifier");
+                        AddModifierAlreadyHasModifier?.Invoke(this, null);
+                        break;
+                    case AddFortModifierResponse.Types.Result.NoItemInInventory:
+                        // PokeStop can't be modified because there is no suitable Items in the player's inventory, there's nothing that we can do
+                        Logger.Write("Adding Pokestop modifier, but there is no item in the player's inventory");
+                        AddModifierNoItemInInventory?.Invoke(this, null);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }, () => true));
+
+        /// <summary>Selects the first available modifier type</summary>
+        /// <returns>Available modifier type or default with count 0</returns>
+        public ItemData SelectAvailableModifier()
+        {
+            var modifier = SelectModifierType(ItemId.ItemTroyDisk);
+            if (modifier != null && modifier.Count != 0)
+                return modifier;
+
+            var fallback = new ItemData { Count = 0, ItemId = ItemId.ItemTroyDisk };
+            return fallback;
+        }
+
+        /// <summary>Selects the modifier type from Inventory</summary>
+        /// <param name="itemId">The item identifier</param>
+        /// <returns>Found modifier or null</returns>
+        private ItemData SelectModifierType(ItemId? itemId)
+        {
+            if (itemId == null)
+                return null;
+
+            var modifier = ItemsInventory?.FirstOrDefault(item => item.ItemId == itemId);
+            if (modifier == null || modifier.Count <= 0)
+                return null;
+
+            return modifier;
+        }
 
         #endregion
 
--- PokemonGo-UWP/Views/SearchPokestopPage.xaml.cs
+++ PokemonGo-UWP/Views/SearchPokestopPage.xaml.cs
@@ -49,6 +49,14 @@ namespace PokemonGo_UWP.Views
             ViewModel.SearchSuccess += GameManagerViewModelOnSearchSuccess;
             // Add also handlers to report which items the user gained after the animation
             SpinPokestopImage.Completed += (s, e) => ShowGatheredItemsMenu.Begin();
+
+            ViewModel.ShowModifierDetails += GameManagerViewModelOnShowModifierDetails;
+            ViewModel.HideModifierDetails += GameManagerViewModelOnHideModifierDetails;
+
+            ViewModel.AddModifierSuccess += GameManagerViewModelOnAddModifierTooFarAwayAddModifierSuccess;
+            ViewModel.AddModifierTooFarAway += GameManagerViewModelOnAddModifierTooFarAway;
+            ViewModel.AddModifierAlreadyHasModifier += GameManagerViewModelOnAddModifierFortAlreadyHasModifier;
+            ViewModel.AddModifierNoItemInInventory += GameManagerViewModelOnAddModifierNoItemInInventory;
         }
 
         private void UnsubscribeToSearchEvents()
@@ -57,6 +65,14 @@ namespace PokemonGo_UWP.Views
             ViewModel.SearchInventoryFull -= GameManagerViewModelOnSearchInventoryFull;
             ViewModel.SearchOutOfRange -= GameManagerViewModelOnSearchOutOfRange;
             ViewModel.SearchSuccess -= GameManagerViewModelOnSearchSuccess;
+
+            ViewModel.ShowModifierDetails -= GameManagerViewModelOnShowModifierDetails;
+            ViewModel.HideModifierDetails -= GameManagerViewModelOnHideModifierDetails;
+
+            ViewModel.AddModifierSuccess -= GameManagerViewModelOnAddModifierTooFarAwayAddModifierSuccess;
+            ViewModel.AddModifierTooFarAway -= GameManagerViewModelOnAddModifierTooFarAway;
+            ViewModel.AddModifierAlreadyHasModifier -= GameManagerViewModelOnAddModifierFortAlreadyHasModifier;
+            ViewModel.AddModifierNoItemInInventory -= GameManagerViewModelOnAddModifierNoItemInInventory;
         }
 
         private void GameManagerViewModelOnSearchOutOfRange(object sender, EventArgs eventArgs)
@@ -80,6 +96,41 @@ namespace PokemonGo_UWP.Views
         {
             SearchPokestopButton.IsEnabled = false;
             SpinPokestopImage.Begin();
+        }
+
+        private void GameManagerViewModelOnShowModifierDetails(object sender, EventArgs eventArgs)
+        {
+            ShowFortModifierDetails.Begin();
+            FortModifierPanel.Visibility = Visibility.Visible;
+            ErrorMessageBorder.Visibility = Visibility.Collapsed;
+            DeployModifierButton.IsEnabled = !ViewModel.IsPokestopLured;
+            DeployModifierButton.Opacity = ViewModel.IsPokestopLured ? .5 : 1;
+        }
+
+        private void GameManagerViewModelOnHideModifierDetails(object sender, EventArgs eventArgs)
+        {
+            HideFortModifierDetails.Begin();
+            FortModifierPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void GameManagerViewModelOnAddModifierTooFarAwayAddModifierSuccess(object sender, EventArgs e)
+        {
+            AddModifierSuccessTextBlock.Visibility = DetailsSuccessMessageBorder.Visibility = Visibility.Visible;
+        }
+
+        private void GameManagerViewModelOnAddModifierTooFarAway(object sender, EventArgs eventArgs)
+        {
+            TooFarAwayTextBlock.Visibility = DetailsErrorMessageBorder.Visibility = Visibility.Visible;
+        }
+
+        private void GameManagerViewModelOnAddModifierFortAlreadyHasModifier(object sender, EventArgs eventArgs)
+        {
+            FortAlreadyHasModifierTextBlock.Visibility = DetailsErrorMessageBorder.Visibility = Visibility.Visible;
+        }
+
+        private void GameManagerViewModelOnAddModifierNoItemInInventory(object sender, EventArgs eventArgs)
+        {
+            NoItemInInventoryTextBlock.Visibility = DetailsErrorMessageBorder.Visibility = Visibility.Visible;
         }
 
         #endregion
